858376765431d9052f416e898e1c8f88f2d075dd
Phillip <renaudtp@oregonstate.edu>
2021-03-02 12:00:03 -0800

    Second Test Written: Multiples of FIVE prints 'Buzz'
    Wrote Second Test, case that tests For the multiples of Five print 'Buzz'

--- test_FizzBuzz.py
+++ test_FizzBuzz.py
@@ -11,5 +11,13 @@ class   TestFizzBuzz(unittest.TestCase):
         self.assertEqual(FizzBuzz.check_Fizz(7), 7)
         self.assertEqual(FizzBuzz.check_Fizz(9), 'Fizz')
 
+    # Test for Buzz -> Multiple of Five
+    def test_Buzz(self):
+        self.assertEqual(FizzBuzz.check_Buzz(5), 5)
+        self.assertEqual(FizzBuzz.check_Buzz(15), 'Buzz')
+        self.assertEqual(FizzBuzz.check_Buzz(20), 20)
+        self.assertEqual(FizzBuzz.check_Buzz(30), 'Buzz')
+        self.assertEqual(FizzBuzz.check_Buzz(35), 35)
+
 if __name__ == '__main__':
     unittest.main()
