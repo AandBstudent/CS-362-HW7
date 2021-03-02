7dec1bd295dd7db6c16b87eead4a788bf5989230
Phillip <renaudtp@oregonstate.edu>
2021-03-02 12:13:21 -0800

    Verify Test 2 Passed: Multiples of Five print "Buzz"
    Implemented method that prints 'Buzz' for multiples FIVE.

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -3,10 +3,18 @@
 # For the multiples of Five print "Buzz"
 # For numbers which are multiples of both Three and Five
 # print "FizzBuzz"
+
 def check_Fizz(x):
     if(x % 3 == 0):
         return 'Fizz'
     else:
         return x
+
+def check_Buzz(x):
+    if(x % 5 == 0):
+        return 'Buzz'
+    else:
+        return x
+
 def Fizz_Buzz():
     pass
