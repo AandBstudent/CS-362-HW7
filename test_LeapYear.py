14484a0d89c49f2c5ecd24c0c9549977a57dae3f
Phillip <renaudtp@oregonstate.edu>
2021-03-02 14:19:03 -0800

    First Test Written Version 2: Detects if year is divisible by Four
    Wrote first test, test to detect if year enter is divisible by Four, Returns True if it is and False if it is not.

--- test_LeapYear.py
+++ test_LeapYear.py
@@ -5,10 +5,10 @@ class TestLeapYear(unittest.TestCase):
 
     # Test to detect years divisible by 4
     def test_DivFour(self):
-        self.assertEqual(LeapYear.LeapYear(2001), 'is not a leap year')
-        self.assertEqual(LeapYear.LeapYear(2020), 'is a leap year')
-        self.assertEqual(LeapYear.LeapYear(1996), 'is a leap year')
-        self.assertEqual(LeapYear.LeapYear(1999), 'is not a leap year')
-        
+        self.assertEqual(LeapYear.DivFour(2001), False)
+        self.assertEqual(LeapYear.DivFour(2020), True)
+        self.assertEqual(LeapYear.DivFour(1996), True)
+        self.assertEqual(LeapYear.DivFour(1999), False)
+
 if __name__ == '__main__':
     unittest.main()
