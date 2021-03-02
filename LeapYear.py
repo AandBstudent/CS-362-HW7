b5d809fca2e8f8bc7b34c4b5527e4089561baae7
Phillip <renaudtp@oregonstate.edu>
2021-03-02 14:22:21 -0800

    Verify Test 1 Passed: Detects if Year is Divisible by Four
    Implemented function to detect if Year is Divisible by Four, If year is return true else return false.

---
+++ LeapYear.py
@@ -0,0 +1,11 @@
+# Conditions for a leap year:
+# Years that are evenly divisble by 4
+# except years that are evenly divisible by 100
+# unless the years are also evenly divisble by 400.
+def DivFour(x):
+    if(x % 4 == 0):
+        return True
+    else:
+        return False
+def LeapYear(x):
+    pass
